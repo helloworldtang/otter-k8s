8298ffc0df5c852bdf1c79804a77a74282f4efb6
唐成 <cheng.tang@gongbangbang.com>
2020-07-29 17:49:58 +0800

    将manager和node分拆成两个Dockerfile，方便进行高可用部署

--- manager/app.sh
+++ manager/app.sh
@@ -4,50 +4,73 @@ set -e
 source /etc/profile
 export JAVA_HOME=/usr/java/latest
 export PATH=$JAVA_HOME/bin:$PATH
-touch /tmp/manager.log
-chown admin: /tmp/manager.log
+touch /tmp/start.log
+chown admin: /tmp/start.log
 chown admin: /home/admin/manager
+host=`hostname -i`
 
-function checkStart() {
-  local name=$1
-  local cmd=$2
-  local timeout=$3
-  cost=5
-  while [ $timeout -gt 0 ]; do
-    ST=$(eval $cmd)
-    if [ "$ST" == "0" ]; then
-      sleep 1
-      let timeout=timeout-1
-      let cost=cost+1
-    elif [ "$ST" == "" ]; then
-      sleep 1
-      let timeout=timeout-1
-      let cost=cost+1
-    else
-      break
-    fi
-  done
-  echo "$name start successful"
+# waitterm
+#   wait TERM/INT signal.
+#   see: http://veithen.github.io/2014/11/16/sigterm-propagation.html
+waitterm() {
+        local PID
+        # any process to block
+        tail -f /dev/null &
+        PID="$!"
+        # setup trap, could do nothing, or just kill the blocker
+        trap "kill -TERM ${PID}" TERM INT
+        # wait for signal, ignore wait exit code
+        wait "${PID}" || true
+        # clear trap
+        trap - TERM INT
+        # wait blocker, ignore blocker exit code
+        wait "${PID}" 2>/dev/null || true
+}
+
+# waittermpid "${PIDFILE}".
+#   monitor process by pidfile && wait TERM/INT signal.
+#   if the process disappeared, return 1, means exit with ERROR.
+#   if TERM or INT signal received, return 0, means OK to exit.
+waittermpid() {
+        local PIDFILE PID do_run error
+        PIDFILE="${1?}"
+        do_run=true
+        error=0
+        trap "do_run=false" TERM INT
+        while "${do_run}" ; do
+                PID="$(cat "${PIDFILE}")"
+                if ! ps -p "${PID}" >/dev/null 2>&1 ; then
+                        do_run=false
+                        error=1
+                else
+                        sleep 1
+                fi
+        done
+        trap - TERM INT
+        return "${error}"
 }
 
 function start_manager() {
-  echo "start manager ..."
-  # start manager
-  su admin -c "cd /home/admin/manager/bin ; sh startup.sh 1>>/tmp/manager.log 2>&1"
-  #check start
-  sleep 5
-  checkStart "manager" "nc 127.0.0.1 8080 -w 1 -z | wc -l" 60
+    echo "start manager ..."
+    # start manager
+    cd /home/admin/manager/bin ; sh startup.sh 1>>/tmp/start.log 2>&1
 }
 
 function stop_manager() {
-  # stop manager
-  echo "stop manager"
-  su admin -c 'cd /home/admin/manager/bin; sh stop.sh 1>>/tmp/manager.log 2>&1'
-  echo "stop manager successful ..."
+    # stop manager
+    echo "stop manager"
+    cd /home/admin/manager/bin; sh stop.sh 1>>/tmp/start.log 2>&1
+    echo "stop manager successful ..."
 }
 
-echo "==> START ..."
 start_manager
-echo "you can visit manager link : http://${OTTER_DOMAIN_NAME}:8080/ , just have fun !"
-
+echo "you can visit manager link : http://$host:8080/ , just have fun !"
 echo "==> START SUCCESSFUL ..."
+
+tail -f /dev/null &
+# wait TERM signal
+waitterm
+
+echo "==> STOP"
+stop_manager
+echo "==> STOP SUCCESSFUL ..."
--- node/app.sh
+++ node/app.sh
@@ -4,59 +4,76 @@ set -e
 source /etc/profile
 export JAVA_HOME=/usr/java/latest
 export PATH=$JAVA_HOME/bin:$PATH
-touch /tmp/node.log
-chown admin: /tmp/node.log
+touch /tmp/start.log
+chown admin: /tmp/start.log
 chown admin: /home/admin/node
-host=$(hostname -i)
+host=`hostname -i`
 
-if [ -z "${MANAGER_ADDRESS}" ]; then
-  MANAGER_ADDRESS="127.0.0.1:8081"
-fi
-if [ -z "${NID}" ]; then
-  NID=1
-fi
-
-function checkStart() {
-  local name=$1
-  local cmd=$2
-  local timeout=$3
-  cost=5
-  while [ $timeout -gt 0 ]; do
-    ST=$(eval $cmd)
-    if [ "$ST" == "0" ]; then
-      sleep 1
-      let timeout=timeout-1
-      let cost=cost+1
-    elif [ "$ST" == "" ]; then
-      sleep 1
-      let timeout=timeout-1
-      let cost=cost+1
-    else
-      break
-    fi
-  done
-  echo "$name start successful"
+# waitterm
+#   wait TERM/INT signal.
+#   see: http://veithen.github.io/2014/11/16/sigterm-propagation.html
+waitterm() {
+        local PID
+        # any process to block
+        tail -f /dev/null &
+        PID="$!"
+        # setup trap, could do nothing, or just kill the blocker
+        trap "kill -TERM ${PID}" TERM INT
+        # wait for signal, ignore wait exit code
+        wait "${PID}" || true
+        # clear trap
+        trap - TERM INT
+        # wait blocker, ignore blocker exit code
+        wait "${PID}" 2>/dev/null || true
 }
 
-function start_node() {
-  echo "start node ..."
-  # start node
-  cmd="sed -i -e 's/^otter.manager.address.*$/otter.manager.address = ${MANAGER_ADDRESS}/' /home/admin/node/conf/otter.properties"
-  eval $cmd
+# waittermpid "${PIDFILE}".
+#   monitor process by pidfile && wait TERM/INT signal.
+#   if the process disappeared, return 1, means exit with ERROR.
+#   if TERM or INT signal received, return 0, means OK to exit.
+waittermpid() {
+        local PIDFILE PID do_run error
+        PIDFILE="${1?}"
+        do_run=true
+        error=0
+        trap "do_run=false" TERM INT
+        while "${do_run}" ; do
+                PID="$(cat "${PIDFILE}")"
+                if ! ps -p "${PID}" >/dev/null 2>&1 ; then
+                        do_run=false
+                        error=1
+                else
+                        sleep 1
+                fi
+        done
+        trap - TERM INT
+        return "${error}"
+}
 
-  su admin -c 'cd /home/admin/node/bin/ && echo '${NID}'> /home/admin/node/conf/nid && sh startup.sh 1>>/tmp/node.log 2>&1'
-  sleep 5
-  #check start
-  checkStart "node" "nc 127.0.0.1 2088 -w 1 -z | wc -l" 30
+
+
+
+function start_node() {
+    echo "start node ..."
+    cd /home/admin/node/bin/ && echo 5 > /home/admin/node/conf/nid && sh startup.sh 1>>/tmp/start.log 2>&1
 }
 
 function stop_node() {
-  # stop node
-  echo "stop node"
-  su admin -c 'cd /home/admin/node/bin/ && sh stop.sh'
-  echo "stop node successful ..."
+    # stop node
+    echo "stop node"
+    cd /home/admin/node/bin/ && sh stop.sh
+    echo "stop node successful ..."
 }
 
 echo "==> START ..."
 start_node
+
 echo "==> START SUCCESSFUL ..."
+
+tail -f /dev/null &
+# wait TERM signal
+waitterm
+
+echo "==> STOP"
+stop_node
+echo "==> STOP SUCCESSFUL ..."
